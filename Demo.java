addd83f636c9a07ab283db5e6d41c938a2a7d850
Vikram201121 <svikram8102@gmail.com>
2024-11-16 16:42:39 +0530

    task320 is commited

--- Demo.java
+++ Demo.java
@@ -1,8 +1,17 @@
 class main
 {
+	//Task-320
+   public static void m1(){
+	int a=10;
+	int b=20;
+	int c=a+b;
+	System.out.print(c);
+   }
+
 	public static void main(String[] args) {
 		
 	}
+	//Task-321
 	public static void m2(){
 		String s="raja";
         String sr="rani";
